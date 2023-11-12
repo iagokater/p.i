ffba4183fbb4c9404284b8befd9f280c9cf932d7
Iagokater <iagokater@live.com>
2023-11-12 15:37:46 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ApplicationRef } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, interval, Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
@@ -20,57 +20,48 @@ export interface IData {
 export class AppComponent implements OnInit, OnDestroy {
   private subscription: Subscription = new Subscription();
 
-  baseUrl: string = 'http:/0.0.0.0:23130'; // Endereço padrão (com IP e porta)
   volumes: IData[] = [];
   alturaAgua: number = 0;
   mediaVolumes: number = 0;
   maiorVolume: number = 0;
   menorVolume: number = 0;
 
-  constructor(private http: HttpClient, private appRef: ApplicationRef) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.restartSubscription();
-  }
+    console.log('Componente inicializado');
 
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
-
-  getDataAPI(): Observable<IData[]> {
-    return this.http.get<IData[]>(`${this.baseUrl}/volume`);
-  }
-
-  calcularEstatisticas() {
-    this.mediaVolumes = Math.floor(
-      this.volumes.reduce((acc, volume) => acc + volume.volume, 0) / this.volumes.length
-    );
-    this.maiorVolume = Math.floor(Math.max(...this.volumes.map((volume) => volume.volume)));
-    this.menorVolume = Math.floor(Math.min(...this.volumes.map((volume) => volume.volume)));
-  }
-
-  onConfigSave() {
-    this.setBaseUrl(this.baseUrl);
-  }
-
-  setBaseUrl(newUrl: string) {
-    this.baseUrl = newUrl;
-    this.restartSubscription();
-  }
-
-  restartSubscription() {
-    this.subscription.unsubscribe();
-    this.subscription = interval(1000)
+    this.subscription = interval(2000)
       .pipe(switchMap(() => this.getDataAPI()))
       .subscribe(
         (response) => {
+          console.log('Resposta da API:', response);
           this.volumes = response;
           this.alturaAgua = this.volumes[0].volume;
           this.calcularEstatisticas();
         },
         (error) => console.error('Erro: ', error)
       );
+  }
+
+  ngOnDestroy() {
+    console.log('Componente destruído');
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  getDataAPI(): Observable<IData[]> {
+    return this.http.get<IData[]>('http://apicollab.dyndns.org:23130/volume');
+  }
+
+  calcularEstatisticas() {
+    this.mediaVolumes = Math.floor(
+      this.volumes.reduce((acc, volume) => acc + volume.volume, 0) / this.volumes.length
+    );
+
+    this.maiorVolume = Math.floor(Math.max(...this.volumes.map((volume) => volume.volume)));
+
+    this.menorVolume = Math.floor(Math.min(...this.volumes.map((volume) => volume.volume)));
   }
 }
